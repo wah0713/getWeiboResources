786209b3eb08c98fa48eb7473d852607c4f13372
wah0713 <wah0713@126.com>
2022-11-19 02:13:27 +0800

    正在进行的任务数

--- index.js
+++ index.js
@@ -2,14 +2,15 @@
 // @name         微博一键取图（9宫格）
 // @namespace    https://github.com/wah0713/getWeiboImage
 // @version      1.02
-// @description  一个兴趣使然的脚本，微博（weibo|wb）一键取图脚本。
+// @description  一个兴趣使然的脚本，微博一键取图脚本。
 // @supportURL   https://github.com/wah0713/getWeiboImage/issues
+// @updateURL    https://greasyfork.org/scripts/454816-%E5%BE%AE%E5%8D%9A%E4%B8%80%E9%94%AE%E5%8F%96%E5%9B%BE-9%E5%AE%AB%E6%A0%BC/code/%E5%BE%AE%E5%8D%9A%E4%B8%80%E9%94%AE%E5%8F%96%E5%9B%BE%EF%BC%889%E5%AE%AB%E6%A0%BC%EF%BC%89.user.js
 // @author       wah0713
 // @compatible   chrome
 // @license      MIT
 // @icon         https://weibo.com/favicon.ico
 // @require      https://cdn.bootcss.com/jquery/1.12.4/jquery.min.js
-// @require      https://cdn.bootcss.com/jszip/3.1.5/jszip.min.js
+// @require      https://cdn.bootcss.com/jszip/3.9.1/jszip.min.js
 // @match        *://weibo.com/*
 // @connect      sinaimg.cn
 // @connect      weibo.com
@@ -24,27 +25,68 @@
 (async function () {
     // 定时器集合
     const timerObject = {}
+    // 消息
+    const message = {
+        getReady: '准备中',
+        noImageError: '失败，未找到图片资源',
+        finish: '完成'
+    }
+    const $frameContent = $('.woo-box-flex.Frame_content_3XrxZ')
+
+    const data = new Proxy({}, {
+        set(target, propKey, value, receiver) {
+            const num = [...Object.keys(target)].length + 1
+            retextDom($frameContent, `进行中的下载任务数：${num}`)
+            return Reflect.set(target, propKey, value, receiver);
+        },
+        deleteProperty(target, propKey) {
+            const num = [...Object.keys(target)].length - 1
+            if (num) {
+                retextDom($frameContent, `进行中的下载任务数：${num}`)
+            } else {
+                retextDom($frameContent, '')
+            }
+            delete target[propKey];
+            return true
+        }
+    })
 
     $('.Main_full_1dfQX').on('click', '.woo-box-flex .head-info_info_2AspQ', async function () {
-        if (gettextDom(this) === '中') return false
-
+        if (![message.noImageError, message.finish, undefined, ''].includes(gettextDom(this))) return false
         const href = $(this).find('.head-info_time_6sFQg').attr('href')
 
-        retextDom(this, '中')
+        data[href] = {
+            num: 0
+        }
+
+        retextDom(this, message.getReady)
         // const imgUrlList = getfileUrlByDom(this)
         const imgUrlList = await getfileUrlByInfo(this)
         if (imgUrlList.length <= 0) {
-            retextDom(this, '失败，未找到图片资源', href)
+            // 没有资源
+            retextDom(this, message.noImageError, href)
+            delete data[href]
             return false
         }
 
-        const promiseList = imgUrlList.map(getFileBlob)
-        const imageRes = await Promise.all(promiseList)
-        retextDom(this, '结束（点击再次下载）', href)
 
-        const writerName = $(this).prev().find('.head_name_24eEB').text()
-        const time = $(this).find('.head-info_time_6sFQg').attr('title') || $(this).find('.head-info_time_6sFQg').text()
-        pack(imageRes, `${writerName}${time}`, )
+        const promiseList = imgUrlList.map((item, index) => getFileBlob(item, index, () => {
+            data[href].num++
+            const total = imgUrlList.length
+            const num = data[href].num
+
+            const percentage = new Intl.NumberFormat(undefined, {
+                maximumFractionDigits: 2
+            }).format(num / total * 100)
+            retextDom(this, `中${num}/ ${total}（${percentage}%）`)
+        }))
+        const imageRes = await Promise.all(promiseList)
+        const writerName = $(this).prev().find('.head_name_24eEB').text().trim()
+        const time = $(this).find('.head-info_time_6sFQg').attr('title').trim() || $(this).find('.head-info_time_6sFQg').text().trim()
+        await pack(imageRes, `${writerName}${time}`, )
+        // 下载成功
+        retextDom(this, message.finish, href)
+        delete data[href]
 
     })
 
@@ -56,20 +98,22 @@
             const name = `${modification}-part${String(obj._id).padStart(2,'0')}${suffixName}`
             zip.file(name, obj._blob);
         });
-
-        // 生成zip文件并下载
-        zip.generateAsync({
-            type: 'blob'
-        }).then((content) => {
-            GM_download({
-                url: URL.createObjectURL(content),
-                name: `${modification}.zip`,
+        return new Promise((resolve, rejcet) => {
+            // 生成zip文件并下载
+            zip.generateAsync({
+                type: 'blob'
+            }).then((content) => {
+                GM_download({
+                    url: URL.createObjectURL(content),
+                    name: `${modification}.zip`,
+                })
+                resolve(content)
             })
         })
     }
 
     // 下载
-    function getFileBlob(url, index) {
+    function getFileBlob(url, index, callback) {
         return new Promise((resolve, rejcet) => {
             GM_xmlhttpRequest({
                 url,
@@ -81,6 +125,7 @@
                 },
                 onload: (res) => {
                     console.log(`getFileBlob-onload`, res)
+                    callback()
                     resolve({
                         ...res,
                         _blob: res.response,
@@ -172,11 +217,11 @@
     }
 
     GM_addStyle(`
-    .woo-box-flex .head-info_info_2AspQ:after{content:"下载" attr(show-text);color:#ff8200;cursor:pointer}
-      `)
+    .woo-box-flex .head-info_info_2AspQ:after{content:"下载" attr(show-text);color:#ff8200;cursor:pointer}.woo-box-flex.Frame_content_3XrxZ:before{content:attr(show-text);color:red;position:fixed;left:0;width:4em}
+    `)
 
     // debugJS
-    unsafeWindow.$ = $
+    // unsafeWindow.$ = $
     // setTimeout(() => {
     // }, 5 * 1000);
 })()
